7a22c93eee62d4315dc88cec296849a0e50f6e7b
Lesley-Byte <phosphorescence3@hotmail.com>
2023-02-23 10:17:41 -0500

    still testing deploy

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,26 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>This is a test. Just a test.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
