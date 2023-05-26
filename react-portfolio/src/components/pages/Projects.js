91f528746729f88261150048b4bbf682492e1bfe
Lesley-Byte <phosphorescence3@hotmail.com>
2023-05-26 16:19:54 -0400

    adding link to wordpress example

--- react-portfolio/src/components/pages/Projects.js
+++ react-portfolio/src/components/pages/Projects.js
@@ -4,6 +4,7 @@ import ByteWriter from "../assets/images/byte-writer.png";
 import ByteSchedule from "../assets/images/byteSchedule.png";
 import ByteWeather from "../assets/images/byteWeather.png";
 import Fade from "react-reveal/Fade";
+import AbstractFlight from "../assets/images/abstract-flight.png";
 
 import BytePortfolio from "../assets/images/screenshot.png";
 const styles = {
@@ -16,6 +17,19 @@ const styles = {
   },
   imageStyles: {
     maxHeight: "fit-content",
+  },
+  overlayText: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    color: "white",
+    fontSize: "2em",
+    fontWeight: "bold",
+    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+    backgroundColor: "rgba(0, 0, 0, 0.6)", // semi-transparent black background
+    padding: "10px", // some padding
+    borderRadius: "5px", // rounded corners
   },
 };
 
@@ -42,6 +56,9 @@ export default function Projects() {
                 style={styles.imageStyles}
               ></img>
               <div class="card-img-overlay"></div>
+              <div class="card-img-overlay">
+                <div style={styles.overlayText}>Heroku MySQL Example</div>
+              </div>
             </div>
           </a>
           <a
@@ -57,6 +74,9 @@ export default function Projects() {
                 style={styles.imageStyles}
               ></img>
               <div class="card-img-overlay"></div>
+              <div class="card-img-overlay">
+                <div style={styles.overlayText}>Heroku Deployment</div>
+              </div>
             </div>
           </a>
           <a
@@ -72,6 +92,9 @@ export default function Projects() {
                 style={styles.imageStyles}
               ></img>
               <div class="card-img-overlay"></div>
+              <div class="card-img-overlay">
+                <div style={styles.overlayText}>Open Weather API</div>
+              </div>
             </div>
           </a>
           <a
@@ -87,6 +110,9 @@ export default function Projects() {
                 style={styles.imageStyles}
               ></img>
               <div class="card-img-overlay"></div>
+              <div class="card-img-overlay">
+                <div style={styles.overlayText}>Bootstrap Example</div>
+              </div>
             </div>
           </a>
           <a
@@ -102,6 +128,26 @@ export default function Projects() {
                 style={styles.imageStyles}
               ></img>
               <div class="card-img-overlay"></div>
+              <div class="card-img-overlay">
+                <div style={styles.overlayText}>Custom CSS</div>
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://lesley-byte.com/"
+            className="card m-3"
+            style={styles.linkStyles}
+          >
+            <div class="card text-bg-dark">
+              <img
+                src={AbstractFlight}
+                className="card-img"
+                alt="..."
+                style={styles.imageStyles}
+              ></img>
+              <div class="card-img-overlay">
+                <div style={styles.overlayText}>WordPress Site</div>
+              </div>
             </div>
           </a>
         </div>
